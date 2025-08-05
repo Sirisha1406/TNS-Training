b2f90067b65b93dc766790dbc54324917058db47
Sirisha1406 <sirishasagar515@gmail.com>
2025-08-05 23:10:25 +0530

    Update DemoTypecasting.java

--- src/DemoTypecasting.java
+++ src/DemoTypecasting.java
@@ -2,11 +2,12 @@
 public class DemoTypecasting {
 
 	public static void main(String[] args) {
+		//Demo for Implicit typecasting
 		int a=20;
 		float b=a;
 		System.out.println(b);
 		
-		//demo for Explicit typecasting
+		//Demo for Explicit typecasting
 		float num=12.0f;
 		int num2=(int)num;
 		System.out.println(num2);
@@ -15,3 +16,4 @@ public class DemoTypecasting {
 	}
 
 }
+
