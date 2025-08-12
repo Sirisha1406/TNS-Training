b673d5f0174ccd1b457eeaae532111dbb44b7222
Babble <Babble@DESKTOP-P2334NO>
2025-08-12 19:07:17 +0530

    conditional statements

---
+++ src/DemoConditionalStatements.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DemoConditionalStatements {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int x=sc.nextInt();
+		if(x%2==0) {
+			System.out.println("The given number is Even!");
+		}
+		else if(x%2==0) {
+			System.out.println("The given number is Odd!");
+		}
+		else {
+			System.out.println("PLease! Enter the integer number!!");
+		}
+
+	}
+
+}
